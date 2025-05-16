722693c30a3160a89566276d55295ae67f7652bb
kpme244r <thunderforce299@gmail.com>
2025-05-17 04:20:04 +0800

    Cleaned up repository and updated listings

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['res.cloudinary.com'], // Add your image hosting domains here
-  },
-}
-
-module.exports = nextConfig 
